aa6ce9728f8ec6d277a883ce77df985086e5d43c
BenaSPACE <BenaSPACE@gmail.com>
2023-07-15 21:32:58 -0400

    job done

---
+++ test.js
@@ -0,0 +1,41 @@
+// alert("hello, world");
+// alert("hello, again!");
+
+// make the image invisible
+//move image form top to bottom
+//make image visible ???
+
+// var myVar = 10;
+// var myVar2 = "Hello";
+// myVar2 = "Hello again";
+// myVar2 = 5;
+// var customerAge = 32;
+// var myVar3 = 40;
+
+// var myVar = 5;
+// 5 + 5;
+// myVar + 10;
+// myVar = myVar + 10;
+// myVar - 3;
+// 5+"bleh";
+// 100+"hi";
+// "Hello"+"world";
+// "Hello"+" world";
+// 5*"hi";
+
+// var myVar = 10;
+// myVar = myVar + 5;
+// myVar += 5;
+// myVar -= 10;
+// myvar /= 2;
+// myVar *= 5;
+// myVar++;
+
+// var myVar = "me";
+// document.write(myVar);
+// console.log(myVar);
+// document.write(10);
+// console.log(50);
+// console.log(20*5);
+// console.log("And as a bonus: ");
+// console.log(31%6);
